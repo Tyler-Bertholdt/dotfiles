252c7fc95df544959bafc75c9547adbfc6fd3a06
Tyler Bertholdt <tyler.1008.errand999@silomails.com>
2025-07-26 13:45:07 +0530

    Auto commit at 2025-07-26 13:45:07

--- hypr/scripts/toggle-gammastep.sh
+++ hypr/scripts/toggle-gammastep.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
 
-STATE_FILE="$HOME/.cache/color_temp_state"
-MIN_TEMP=3500
-MAX_TEMP=6500
-STEP=250
+PID=$(pgrep -x gammastep)
 
-# Init if missing
-if [[ ! -f "$STATE_FILE" ]]; then
-    echo "$MAX_TEMP" > "$STATE_FILE"
-fi
-
-CUR_TEMP=$(cat "$STATE_FILE")
-
-if [[ "$1" == "down" ]]; then
-    NEW_TEMP=$((CUR_TEMP - STEP))
-    [[ $NEW_TEMP -lt $MIN_TEMP ]] && NEW_TEMP=$MIN_TEMP
-elif [[ "$1" == "up" ]]; then
-    NEW_TEMP=$((CUR_TEMP + STEP))
-    [[ $NEW_TEMP -gt $MAX_TEMP ]] && NEW_TEMP=$MAX_TEMP
+if [ -n "$PID" ]; then
+    kill "$PID"  
 else
-    exit 1
+    gammastep  4500 &  
 fi
-
-gammastep -O "$NEW_TEMP"
-echo "$NEW_TEMP" > "$STATE_FILE"
