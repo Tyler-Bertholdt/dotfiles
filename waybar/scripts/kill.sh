2fae734107937f4cf24c5680321347f30d5709c7
Tyler Bertholdt <tyler.1008.errand999@silomails.com>
2025-08-10 21:48:35 +0530

    Auto commit at 2025-08-10 21:48:35

--- waybar/scripts/kill.sh
+++ waybar/scripts/kill.sh
@@ -2,7 +2,7 @@
 killall waybar
 killall nwg-dock-hyprland
 waybar &disown 
-nwg-dock-hyprland -w 5 -p bottom -mb 12 -lp start -l top -i 40 -ico ~/Server/manjaro-vector-logo-seeklogo/manjaro-seeklogo.png & disown
-n
+nwg-dock-hyprland -w 5 -p bottom -mb 12 -lp start -l top -i 40 -ico ~/Server/manjaro-vector-logo-seeklogo/manjaro-seeklogo.png -c 'nwg-drawer -mb 80 -ml 100 -mt 80 -mr 100 -ovl -is 50 -closebtn right -spacing 10 -c 6' & disown
+
  
 
