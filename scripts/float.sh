44ce882dffcac5f07b6fa8837666e11d15473cae
Tyler Bertholdt <tyler.1008.errand999@silomails.com>
2025-09-12 09:41:03 +0530

    Auto commit at 2025-09-12 09:41:02

--- scripts/float.sh
+++ scripts/float.sh
@@ -1,13 +1,15 @@
+
 #!/bin/bash
 
-hyprctl dispatch centerwindow
-hyprctl dispatch resizeactive exact 1345 750
-pkill -SIGUSR1 waybar
+dock_pids=$(pgrep -f "nwg-dock-hyprland")
 
-dock_pid=$(pidof nwg-dock-hyprland)
-
-if [[ -n "$dock_pid" ]]; then
-    kill "$dock_pid"
+if [[ -n "$dock_pids" ]]; then
+    # If dock is running: toggle visibility
+    for pid in $dock_pids; do
+        kill -SIGRTMIN+1 "$pid"
+    done
 else
-nwg-dock-hyprland -w 5 -p bottom -mb 7 -lp start -l overlay -i 56 -ico ~/File_Save/manjaro-vector-logo-seeklogo/manjaro-seeklogo.png -c 'nwg-drawer -mb 0 -ml 0 -mt 0 -mr 0 -ovl -is 58 -closebtn none -spacing 30 -c 7 -nocats' & disown 
+    # Start the dock normally (shown mode)
+    nwg-dock-hyprland -w 5 -p bottom -mb 7 -lp start -l overlay -i 56 -ico ~/File_Save/manjaro-vector-logo-seeklogo/manjaro-seeklogo.png -c 'nwg-drawer -mb 0 -ml 0 -mt 0 -mr 0 -ovl -is 58 -closebtn none -spacing 30 -c 7 -nocats' & disown
 fi
+
--- scripts/pin.sh
+++ scripts/pin.sh
@@ -1,10 +1,14 @@
+
 #!/bin/bash
 
-# Check if nwg-dock-hyprland is running
-if pgrep -x "nwg-dock-hyprland" > /dev/null; then
-    # Send SIGUSR1 to toggle visibility
-    pkill -USR1 -x "nwg-dock-hyprland"
-else
-    # Start nwg-dock-hyprland with autohide enabled
-    nwg-dock-hyprland -d &
+dock_pids=$(pgrep -f "nwg-dock-hyprland")
+
+if [[ -n "$dock_pids" ]]; then
+    for pid in $dock_pids; do
+        kill "$pid"
+        sleep 0.1  
+    done
 fi
+
+nwg-dock-hyprland -d -hd 0 -hl overlay -w 5 -p bottom -mb 7 -lp start -l overlay -i 56 -ico ~/File_Save/manjaro-vector-logo-seeklogo/manjaro-seeklogo.png -c 'nwg-drawer -mb 0 -ml 0 -mt 0 -mr 0 -ovl -is 58 -closebtn none -spacing 30 -c 7 -nocats' & disown
+
